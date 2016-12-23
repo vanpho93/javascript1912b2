8858b746e5b7e10ae40a3688d1d932ffffe48b85
Admin <vanpho02@gmail.com>
2016-12-23 20:30:33 +0700

    function

--- b2.js
+++ b2.js
@@ -2,8 +2,14 @@ function TamGiac(a, b, c){
   this.a = a;
   this.b = b;
   this.c = c;
+  this.chuVi = getChuVi2(this.a, this.b, this.c);
   this.getChuVi = () => this.a + this.b + this.c
+  function getChuVi2(c1, c2, c3){//private method
+    return c1 + c2 + c3;
+  }
 }
 
 var tg = new TamGiac(3, 4, 3);
-console.log(tg.getChuVi());
+// console.log(tg.getChuVi());
+
+console.log(tg);
