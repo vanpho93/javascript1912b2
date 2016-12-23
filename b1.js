0936a4baa21324c25327f1eb581a092dddd56581
Admin <vanpho02@gmail.com>
2016-12-23 20:02:45 +0700

    xong

--- b1.js
+++ b1.js
@@ -1,2 +1,28 @@
-let person = {name: 'Khoa Pham', age: 18};
-console.log(person);
+let name = 'Khoa Pham'
+let person1 = {name: name, age: 18};
+//console.log(person);
+
+function Person(name, age){
+  this.name = name;
+  this.age = age;
+  this.sayHello = () => console.log('Xin chao');
+  this.get = getInfo;
+  function getInfo(){
+    return this.name + ': ' + this.age;
+  }
+}
+
+var p1 = new Person('Khoa Pham', 18);
+p1.sayHello();
+console.log(p1.get());
+console.log('Ten', p1.name);
+console.log(p1);
+
+function KhoaHoc(tenKH, hocPhi){
+  this.tenKH = tenKH;
+  this.hocPhi = hocPhi;
+  this.soHocVien = 10;
+}
+
+var node = new KhoaHoc('NodeJS', '3000');
+// console.log(node);
